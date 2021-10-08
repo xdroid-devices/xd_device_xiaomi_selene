129701d24e9cab96c912017f2377a7fa32bb21eb
TH779 <i@779.moe>
2021-10-08 09:13:08 +0800

    selene: rootdir: Take logs without ADB

--- device.mk
+++ device.mk
@@ -380,6 +380,7 @@ PRODUCT_PACKAGES += \
     factory_init.connectivity.rc \
     factory_init.project.rc \
     factory_init.rc \
+    init.logcat.rc \
     init.ago.rc \
     init.connectivity.rc \
     init.mi_thermald.rc \
