38c49f3dc7e8342b9e700ab613e24f30b518fd8c
Mikazuuu07 <irfn4566@gmail.com>
2022-01-21 14:54:37 +0000

    selene: Build HAL sensorservice 1.0
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -335,6 +335,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     libsensorndkbridge \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.0.vendor
